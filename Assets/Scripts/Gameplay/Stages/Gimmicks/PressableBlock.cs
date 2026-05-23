806e26763fdabc48262d0f70741dca726b1de3d1
tkgpoyo <tkgpoyo@gmail.com>
2026-05-24 01:30:59 +0900

    前方の衝突のみを検知

--- Assets/Scripts/Gameplay/Stages/Gimmicks/PressableBlock.cs
+++ Assets/Scripts/Gameplay/Stages/Gimmicks/PressableBlock.cs
@@ -13,6 +13,9 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
     [RequireComponent(typeof(Collider2D))]
     public class PressableBlock : MonoBehaviour
     {
+        /// <summary>ブロック前方に障害物があるかどうかを確認する際の余白</summary>
+        private const float CHECK_MARGIN = 0.1f;
+
         /// <summary>ブロックが動くはやさ</summary>
         [SerializeField] private float _speed = 1f;
         /// <summary>押す際の障害物レイヤー</summary>
@@ -21,6 +24,7 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
         [SerializeField] private float _moveDistance = 1f;
 
         private Collider2D _collider;
+        private bool _isMoving = false;
 
         void Awake()
         {
@@ -41,17 +45,14 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
         /// <param name="dir">動かす方向</param>
         public void Move(EBlockMoveDirection dir)
         {
+            if (_isMoving) { return; }
+
             var direction = dir switch
             {
                 EBlockMoveDirection.Right => Vector2.right,
                 EBlockMoveDirection.Left => Vector2.left,
                 _ => Vector2.zero
             };
-
-            if (!CanMove(direction, _moveDistance))
-            {
-                return;
-            }
 
             var targetX = ((Vector2)transform.position + direction * _moveDistance).x;
             StartCoroutine(MoveCoroutine(targetX));
@@ -64,21 +65,30 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
         /// <returns></returns>
         private IEnumerator MoveCoroutine(float targetX)
         {
+            _isMoving = true;
+
             var currentPos = (Vector2)transform.position;               // 現在の位置を取得
             var targetPos = new Vector2(targetX, currentPos.y);         // 目標位置
             while (!Mathf.Approximately(currentPos.x, targetPos.x))     // 目標位置に達するまで
             {
-                currentPos = Vector2.MoveTowards(
+                var nextPos = Vector2.MoveTowards(
                     currentPos,
                     targetPos,
                     _speed * Time.fixedDeltaTime
                 );                                                      // 現在の位置を更新
+                var moveVec = nextPos - currentPos;                     // 移動方向
+                if (!CanMove(moveVec.normalized, moveVec.magnitude))    // 動かせない場合
+                {
+                    break;                                              // 移動を終了
+                }
+                currentPos = nextPos;
                 transform.position = currentPos;                        // 位置を更新
 
                 yield return new WaitForFixedUpdate();                  // 1フレーム待つ
             }
 
-            transform.position = targetPos;                             // 最終位置を目標位置に揃える
+            transform.position = currentPos;                            // 最終位置を目標位置に揃える
+            _isMoving = false;
         }
 
         /// <summary>
@@ -92,7 +102,7 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
             for (var i = 0; i < contactCount; i++)
             {
                 var contact = collision.GetContact(i);                  // 接触情報を取得
-                if (contact.normal.y > 0.5f)                            // 法線が下向きであれば
+                if (contact.normal.y > 0.5f)                            // 法線が上向きであれば
                 {
                     return true;                                        // 下で接触したとする
                 }
@@ -105,25 +115,36 @@ namespace MaruSikaku.Gameplay.Stages.Gimmicks
         /// </summary>
         /// <param name="direction">動かす方向</param>
         /// <param name="distance">動かす距離</param>
-        /// <returns></returns>
+        /// <returns>動かせるかどうか</returns>
         private bool CanMove(Vector2 direction, float distance)
         {
-            var hits = new RaycastHit2D[8];     // ヒット情報の配列
-            var filter = new ContactFilter2D
-            {
-                useLayerMask = true,
-                layerMask = _obstacleLayer,
-                useTriggers = false
-            };                                  //  衝突判定のFilter
+            var bounds = _collider.bounds;
+            var size = new Vector2(CHECK_MARGIN, bounds.size.y * 0.8f);
 
-            var hitCount = _collider.Cast(
+            var originX = direction.x > 0 ? 
+                bounds.max.x + CHECK_MARGIN * 0.5f :
+                bounds.min.x - CHECK_MARGIN * 0.5f;     // 前方の障害物判定の当たり判定Boxの中心X座標
+            var origin = new Vector2(originX, bounds.center.y);
+
+            var hits = Physics2D.BoxCastAll(
+                origin,
+                size,
+                0f,
                 direction,
-                filter,
-                hits,
-                distance
-            );                                  // 移動先の障害物の個数
+                distance,
+                _obstacleLayer
+            );
 
-            return hitCount == 0;               // 移動先に障害物がなければ動かせるとする
+            foreach (var hit in hits)
+            {
+                if (hit.collider == null) { continue; }
+                if (hit.collider == _collider) { continue; }
+                if (hit.collider.transform.IsChildOf(transform)) { continue; }
+
+                return false;
+            }
+
+            return true;
         }
     }
 }
